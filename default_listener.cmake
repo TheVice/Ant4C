ca4d76e60f3767fc0dc667bb92a0ac9833bd8e4c
TheVice <thewinlab@hotmail.com>
2020-09-25 15:28:05 +0300

    [develop] synchronizate data from the release branch.
    Fixed typo at CHANGELOG, filled checksums.

--- default_listener.cmake
+++ default_listener.cmake
@@ -2,3 +2,8 @@
 add_library(default_listener SHARED
             "${CMAKE_SOURCE_DIR}/default_listener.c"
             "${CMAKE_SOURCE_DIR}/default_listener.h")
+
+if(("GNU" STREQUAL "${CMAKE_CXX_COMPILER_ID}") AND (NOT MINGW))
+  target_compile_options(default_listener PRIVATE "-fPIE")
+  set_target_properties(default_listener PROPERTIES LINK_FLAGS "-pie -Wl,-z,now")
+endif()
