b7e4a0fdafd1332f29010881d1b1d66b06279b24
TheVice <thewinlab@hotmail.com>
2021-03-15 17:52:22 +0200

    [ant4c.net.module] added file::is-assembly function.
    Supported fx host 3.1 and above.

--- ant4c.net.module.c
+++ ant4c.net.module.c
@@ -11,6 +11,7 @@
 #include "common.h"
 #include "conversion.h"
 #include "file_system.h"
+#include "path.h"
 #include "range.h"
 #include "string_unit.h"
 #include "text_encoding.h"
@@ -1037,6 +1038,17 @@ uint8_t hostfxr_get_native_search_directories(
 #endif
 }
 
+static const uint8_t* types_of_delegate[] =
+{
+	(const uint8_t*)"host_fxr_hdt_com_activation",
+	(const uint8_t*)"host_fxr_hdt_load_in_memory_assembly",
+	(const uint8_t*)"host_fxr_hdt_winrt_activation",
+	(const uint8_t*)"host_fxr_hdt_com_register",
+	(const uint8_t*)"host_fxr_hdt_com_unregister",
+	(const uint8_t*)"host_fxr_hdt_load_assembly_and_get_function_pointer",
+	(const uint8_t*)"host_fxr_hdt_get_function_pointer"
+};
+
 uint8_t hostfxr_get_runtime_delegate(
 	void* ptr_to_host_fxr_object,
 	const uint8_t** values, const uint16_t* values_lengths, uint8_t values_count,
@@ -1051,17 +1063,6 @@ uint8_t hostfxr_get_runtime_delegate(
 		return 0;
 	}
 
-	static const uint8_t* types_of_delegate[] =
-	{
-		(const uint8_t*)"host_fxr_hdt_com_activation",
-		(const uint8_t*)"host_fxr_hdt_load_in_memory_assembly",
-		(const uint8_t*)"host_fxr_hdt_winrt_activation",
-		(const uint8_t*)"host_fxr_hdt_com_register",
-		(const uint8_t*)"host_fxr_hdt_com_unregister",
-		(const uint8_t*)"host_fxr_hdt_load_assembly_and_get_function_pointer",
-		(const uint8_t*)"host_fxr_hdt_get_function_pointer"
-	};
-	/**/
 	const void* context = string_to_pointer(
 							  values[0], (uint8_t)values_lengths[0], output);
 
@@ -1781,23 +1782,284 @@ uint8_t hostfxr_set_runtime_property_value(
 	return 1;
 }
 
-/*uint8_t file_is_assembly(const uint8_t* path, uint16_t length, uint8_t* returned)
+static const uint8_t* name_spaces[] =
 {
-	if (!path ||
-		!length ||
-		!returned)
+	(const uint8_t*)"nethost",
+	(const uint8_t*)"hostfxr",
+	(const uint8_t*)"file"
+};
+
+static const uint8_t* all_functions[][21] =
+{
+	{ (const uint8_t*)"get-hostfxr-path", NULL },
+	{
+		(const uint8_t*)"functions",
+		(const uint8_t*)"initialize",
+		(const uint8_t*)"is-function-exists",
+		(const uint8_t*)"result-to-string",
+		/**/
+		(const uint8_t*)"close",
+		(const uint8_t*)"get-available-sdks",
+		(const uint8_t*)"get-native-search-directories",
+		(const uint8_t*)"get-runtime-delegate",
+		(const uint8_t*)"get-runtime-properties",
+		(const uint8_t*)"get-runtime-property-value",
+		(const uint8_t*)"initialize-for-dotnet-command-line",
+		(const uint8_t*)"initialize-for-runtime-config",
+		(const uint8_t*)"main",
+		(const uint8_t*)"main-bundle-startupinfo",
+		(const uint8_t*)"main-startupinfo",
+		(const uint8_t*)"resolve-sdk",
+		(const uint8_t*)"resolve-sdk2",
+		(const uint8_t*)"run-app",
+		(const uint8_t*)"set-error-writer",
+		(const uint8_t*)"set-runtime-property-value",
+		NULL
+	},
+	{ (const uint8_t*)"is-assembly", NULL },
+};
+
+enum ant4c_net_module_
+{
+	net_host_get_hostfxr_path_,
+	/**/
+	hostfxr_functions_,
+	hostfxr_initialize_,
+	hostfxr_is_function_exists_,
+	hostfxr_result_to_string_,
+	/**/
+	hostfxr_close_,
+	hostfxr_get_available_sdks_,
+	hostfxr_get_native_search_directories_,
+	hostfxr_get_runtime_delegate_,
+	hostfxr_get_runtime_properties_,
+	hostfxr_get_runtime_property_value_,
+	hostfxr_initialize_for_dotnet_command_line_,
+	hostfxr_initialize_for_runtime_config_,
+	hostfxr_main_,
+	hostfxr_main_bundle_startupinfo_,
+	hostfxr_main_startupinfo_,
+	hostfxr_resolve_sdk_,
+	hostfxr_resolve_sdk2_,
+	hostfxr_run_app_,
+	hostfxr_set_error_writer_,
+	hostfxr_set_runtime_property_value_,
+	/**/
+	file_is_assembly_
+};
+
+uint8_t file_is_assembly(
+	void* ptr_to_host_fxr_object,
+	const uint8_t** values, const uint16_t* values_lengths, uint8_t values_count,
+	struct buffer* output)
+{
+	struct assembly_argument
+	{
+		const type_of_element* path;
+	};
+	/**/
+	typedef uint8_t(delegate_calling_convention * custom_entry_point_fn)(struct assembly_argument args);
+
+	if (!values ||
+		!values_lengths ||
+		(1 != values_count && 2 != values_count) ||
+		!output)
 	{
 		return 0;
 	}
 
-	if (!metahost_get_clr_version_from_file(path, length))
+	const void* the_delegate = NULL;
+
+	if (1 == values_count)
+	{
+		if (!ptr_to_host_fxr_object)
+		{
+			return 0;
+		}
+
+		const uint8_t* get_runtime_delegate = all_functions[1][7];
+		values_count = (uint8_t)common_count_bytes_until(get_runtime_delegate, 0);
+
+		if (!hostfxr_is_function_exists(ptr_to_host_fxr_object, name_spaces[1],
+										get_runtime_delegate, values_count, output))
+		{
+			return 0;
+		}
+
+		if (!bool_parse(buffer_data(output, 0), buffer_size(output), &values_count))
+		{
+			return 0;
+		}
+
+		if (!buffer_resize(output, 0))
+		{
+			return 0;
+		}
+
+		if (values_count)
+		{
+			static const uint8_t* json_file_content =
+				(const uint8_t*)""								\
+				"{\n"											\
+				"  \"runtimeOptions\": {\n"						\
+				"    \"tfm\": \"netcoreapp3.1\",\n"				\
+				"    \"rollForward\": \"LatestMinor\",\n"		\
+				"    \"framework\": {\n"						\
+				"      \"name\": \"Microsoft.NETCore.App\",\n"	\
+				"      \"version\": \"3.1.0\" \n"				\
+				"    }\n"										\
+				"  }\n"											\
+				"}\n";
+
+			if (!common_append_string_to_buffer(json_file_content, output))
+			{
+				return 0;
+			}
+
+			values_count = (uint8_t)buffer_size(output);
+
+			if (!path_get_temp_file_name(output))
+			{
+				return 0;
+			}
+
+			if (!buffer_resize(output, buffer_size(output) - 2) ||
+				!common_append_string_to_buffer((const uint8_t*)".json", output) ||
+				!buffer_push_back(output, 0))
+			{
+				return 0;
+			}
+
+			const uint8_t* sub_values[] = { NULL, NULL, buffer_data(output, values_count), NULL, NULL, NULL };
+			uint16_t sub_values_lengths[] = { 0, 0, (uint16_t)(buffer_size(output) - values_count), 0, 0, 0 };
+
+			if (!buffer_resize(output, values_count))
+			{
+				return 0;
+			}
+
+			if (!file_write_all(sub_values[2], output))
+			{
+				return 0;
+			}
+
+			struct buffer sub_output;
+
+			SET_NULL_TO_BUFFER(sub_output);
+
+			if (!hostfxr_initialize_for_runtime_config(
+					ptr_to_host_fxr_object,
+					sub_values, sub_values_lengths, 3, &sub_output))
+			{
+				buffer_release(&sub_output);
+				return 0;
+			}
+
+			sub_values[0] = buffer_data(&sub_output, 0);
+
+			if (!sub_values[0] ||
+				0 == *(sub_values[0]))
+			{
+				buffer_release(&sub_output);
+				return 0;
+			}
+
+			const void* context = pointer_parse(sub_values[0]);
+
+			if (!buffer_resize(output, 0) ||
+				!buffer_append_data_from_buffer(output, &sub_output))
+			{
+				host_fxr_close(ptr_to_host_fxr_object, context);
+				buffer_release(&sub_output);
+				return 0;
+			}
+
+			sub_values[0] = buffer_data(output, 0);
+			sub_values[1] = types_of_delegate[host_fxr_hdt_load_assembly_and_get_function_pointer];
+			sub_values[2] = (const uint8_t*)"ant4c.net.module.clr.dll";
+			sub_values[3] = (const uint8_t*)"Ant4C.Net.Module.Delegates, ant4c.net.module.clr";
+			sub_values[4] = (const uint8_t*)"FileUnit_IsAssembly";
+			sub_values[5] = (const uint8_t*)
+							"Ant4C.Net.Module.Delegates+FileUnit_IsAssemblyDelegate, ant4c.net.module.clr";
+			/**/
+			sub_values_lengths[0] = (uint16_t)buffer_size(output);
+
+			for (uint8_t i = 1; i < 6; ++i)
+			{
+				sub_values_lengths[i] = (uint16_t)common_count_bytes_until(sub_values[i], 0);
+			}
+
+			if (!buffer_resize(&sub_output, 0) ||
+				!hostfxr_get_runtime_delegate(ptr_to_host_fxr_object, sub_values, sub_values_lengths, 6, &sub_output))
+			{
+				host_fxr_close(ptr_to_host_fxr_object, context);
+				buffer_release(&sub_output);
+				return 0;
+			}
+
+			sub_values[0] = buffer_data(&sub_output, 0);
+
+			if (!sub_values[0] ||
+				0 == *(sub_values[0]))
+			{
+				host_fxr_close(ptr_to_host_fxr_object, context);
+				buffer_release(&sub_output);
+				return 0;
+			}
+
+			the_delegate = pointer_parse(sub_values[0]);
+			buffer_release(&sub_output);
+
+			if (HOST_FX_RESOLVER_NON_SUCCESS(host_fxr_close(ptr_to_host_fxr_object, context)))
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			/*TODO*/
+		}
+	}
+	else
+	{
+		the_delegate = string_to_pointer(values[1], (uint8_t)values_lengths[1], output);
+	}
+
+	if (!the_delegate)
 	{
 		return 0;
 	}
 
-	*returned = 0 < buffer_size(&output_data);
-	return buffer_resize(&output_data, 0);
-}*/
+	if (!buffer_resize(output, 0) ||
+		!value_to_system_path(values[0], values_lengths[0], output))
+	{
+		return 0;
+	}
+
+	struct assembly_argument custom_argument;
+
+#if defined(_WIN32)
+	custom_argument.path = (const type_of_element*)buffer_data(output, (ptrdiff_t)1 + values_lengths[0]);
+
+#else
+	custom_argument.path = (const type_of_element*)buffer_data(output, 0);
+
+#endif
+	if (!custom_argument.path)
+	{
+		return 0;
+	}
+
+	const uint8_t is_assembly = ((custom_entry_point_fn)the_delegate)(custom_argument);
+
+	if (!buffer_resize(output, 0) ||
+		!bool_to_string(is_assembly, output))
+	{
+		return 0;
+	}
+
+	return 1;
+}
 
 void* error_writer = NULL;
 
@@ -1897,72 +2159,6 @@ void host_fx_resolver_sdk2(int32_t key, const type_of_element* value)
 		buffer_resize(&output_data, 0);
 	}
 }
-
-static const uint8_t* name_spaces[] =
-{
-	(const uint8_t*)"nethost",
-	(const uint8_t*)"hostfxr",
-	(const uint8_t*)"file"
-};
-
-static const uint8_t* all_functions[][21] =
-{
-	{ (const uint8_t*)"get-hostfxr-path", NULL },
-	{
-		(const uint8_t*)"functions",
-		(const uint8_t*)"initialize",
-		(const uint8_t*)"is-function-exists",
-		(const uint8_t*)"result-to-string",
-		/**/
-		(const uint8_t*)"close",
-		(const uint8_t*)"get-available-sdks",
-		(const uint8_t*)"get-native-search-directories",
-		(const uint8_t*)"get-runtime-delegate",
-		(const uint8_t*)"get-runtime-properties",
-		(const uint8_t*)"get-runtime-property-value",
-		(const uint8_t*)"initialize-for-dotnet-command-line",
-		(const uint8_t*)"initialize-for-runtime-config",
-		(const uint8_t*)"main",
-		(const uint8_t*)"main-bundle-startupinfo",
-		(const uint8_t*)"main-startupinfo",
-		(const uint8_t*)"resolve-sdk",
-		(const uint8_t*)"resolve-sdk2",
-		(const uint8_t*)"run-app",
-		(const uint8_t*)"set-error-writer",
-		(const uint8_t*)"set-runtime-property-value",
-		NULL
-	},
-	{ (const uint8_t*)"is-assembly", NULL },
-};
-
-enum ant4c_net_module_
-{
-	net_host_get_hostfxr_path_,
-	/**/
-	hostfxr_functions_,
-	hostfxr_initialize_,
-	hostfxr_is_function_exists_,
-	hostfxr_result_to_string_,
-	/**/
-	hostfxr_close_,
-	hostfxr_get_available_sdks_,
-	hostfxr_get_native_search_directories_,
-	hostfxr_get_runtime_delegate_,
-	hostfxr_get_runtime_properties_,
-	hostfxr_get_runtime_property_value_,
-	hostfxr_initialize_for_dotnet_command_line_,
-	hostfxr_initialize_for_runtime_config_,
-	hostfxr_main_,
-	hostfxr_main_bundle_startupinfo_,
-	hostfxr_main_startupinfo_,
-	hostfxr_resolve_sdk_,
-	hostfxr_resolve_sdk2_,
-	hostfxr_run_app_,
-	hostfxr_set_error_writer_,
-	hostfxr_set_runtime_property_value_,
-	/**/
-	file_is_assembly_
-};
 
 const uint8_t* enumerate_name_spaces(ptrdiff_t index)
 {
@@ -2432,64 +2628,15 @@ uint8_t evaluate_function(
 			}
 
 			break;
-#if 0
 
 		case file_is_assembly_:
-			if (1 != values_count ||
-				!file_is_assembly(values[0], values_lengths[0], &values_count) ||
-				!bool_to_string(values_count, &output_data))
+			if (!file_is_assembly(ptr_to_host_fxr_object,
+								  values, values_lengths, values_count, &output_data))
 			{
 				return 0;
 			}
 
 			break;
-
-		case framework_exists_:
-			if (1 != values_count ||
-				!text_encoding_UTF8_to_UTF16LE(values[0], values[0] + values_lengths[0], &output_data) ||
-				!framework_get_function_return(buffer_wchar_t_data(&output_data, 0)))
-			{
-				return 0;
-			}
-
-			break;
-
-		case framework_get_clr_version_:
-			if (0 < values_count ||
-				!FRAMEWORK_GET_CLR_VERSION())
-			{
-				return 0;
-			}
-
-			break;
-
-		case framework_get_framework_directory_:
-			if (1 < values_count ||
-				!FRAMEWORK_GET_FRAMEWORK_DIRECTORY())
-			{
-				return 0;
-			}
-
-			break;
-
-		case framework_get_frameworks_:
-			if (1 < values_count ||
-				!FRAMEWORK_GET_FRAMEWORKS())
-			{
-				return 0;
-			}
-
-			break;
-
-		case framework_get_runtime_framework_:
-			if (0 < values_count ||
-				!framework_get_function_return(NULL))
-			{
-				return 0;
-			}
-
-			break;
-#endif
 
 		default:
 			return 0;
