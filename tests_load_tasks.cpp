TheVice/Ant4C

commit 735341472f6db74a2aecae9e589fd3f7588daff9

[*.xml] added approach to load modules with 'dylib' extension.
[tests_load_tasks] save 'full_path_to_module' in std::string variable at
'load_tasks_evaluate_task' and 'project_load_from_build_file' tests.

tests_load_tasks.cpp
@@ -127,11 +127,14 @@ TEST_F(TestLoadTasks, load_tasks_evaluate_task)
 			continue;
 		}
 
+		const std::string full_path_to_module(reinterpret_cast<const char*>(full_path));
 		static const void* the_target = nullptr;
 		const auto returned = load_tasks_evaluate_task(&the_project, the_target, &task_arguments, verbose);
 		//
 		ASSERT_EQ(expected_return, returned)
-				<< buffer_free_with_inner_buffers(&task_arguments) << std::endl << project_free(&the_project);
+				<< full_path_to_module
+				<< buffer_free_with_inner_buffers(&task_arguments) << std::endl
+				<< project_free(&the_project);
 		//
 		project_clear(&the_project);
 		//
@@ -167,17 +170,17 @@ TEST_F(TestLoadTasks, project_load_from_build_file)
 	for (const auto& node : nodes)
 	{
 		const auto name_of_module(node.node().select_node("module").node().child_value());
-		auto full_path = path_to_directory_with_image + name_of_module;
+		auto full_path_to_module = path_to_directory_with_image + name_of_module;
 
-		if (!file_exists(reinterpret_cast<const uint8_t*>(full_path.c_str())) &&
+		if (!file_exists(reinterpret_cast<const uint8_t*>(full_path_to_module.c_str())) &&
 			path_to_directory_with_image != current_directory)
 		{
-			full_path = current_directory + name_of_module;
+			full_path_to_module = current_directory + name_of_module;
 
-			if (!file_exists(reinterpret_cast<const uint8_t*>(full_path.c_str())))
+			if (!file_exists(reinterpret_cast<const uint8_t*>(full_path_to_module.c_str())))
 			{
 				std::cout << __FUNCTION__ << "[" << __LINE__ << "]:" << " file" << std::endl;
-				std::cout << "'" << full_path << "'" << std::endl;
+				std::cout << "'" << full_path_to_module << "'" << std::endl;
 				std::cout << "doesn't exist." << std::endl;
 				std::cout << __FUNCTION__ << ": test case " << node_count << " will be skip." << std::endl;
 				//
@@ -187,12 +190,12 @@ TEST_F(TestLoadTasks, project_load_from_build_file)
 		}
 
 		const auto name_of_build_file(node.node().select_node("build_file").node().child_value());
-		full_path = path_to_source + name_of_build_file;
+		const auto full_path_to_script_file = path_to_source + name_of_build_file;
 
-		if (!file_exists(reinterpret_cast<const uint8_t*>(full_path.c_str())))
+		if (!file_exists(reinterpret_cast<const uint8_t*>(full_path_to_script_file.c_str())))
 		{
 			std::cout << __FUNCTION__ << "[" << __LINE__ << "]:" << " file" << std::endl;
-			std::cout << "'" << full_path << "'" << std::endl;
+			std::cout << "'" << full_path_to_script_file << "'" << std::endl;
 			std::cout << "doesn't exist." << std::endl;
 			std::cout << __FUNCTION__ << ": test case " << node_count << " will be skip." << std::endl;
 			//
@@ -204,27 +207,30 @@ TEST_F(TestLoadTasks, project_load_from_build_file)
 		const auto target_name_in_range(string_to_range(target_name));
 		//
 		project_clear(&the_project);
-		const auto path_to_build_file(string_to_range(full_path));
+		const auto path_to_build_file(string_to_range(full_path_to_script_file));
 		static const uint8_t project_help = 0;
 		//
 		ASSERT_TRUE(project_load_from_build_file(
 						&path_to_build_file, &current_directory_in_range,
 						Default, &the_project, project_help, verbose))
-				<< full_path << std::endl
+				<< full_path_to_module << std::endl
+				<< full_path_to_script_file << std::endl
 				<< current_directory << std::endl
 				<< project_free(&the_project);
 
 		if (range_is_null_or_empty(&target_name_in_range))
 		{
 			ASSERT_TRUE(project_evaluate_default_target(&the_project, verbose))
-					<< full_path << std::endl
+					<< full_path_to_module << std::endl
+					<< full_path_to_script_file << std::endl
 					<< current_directory << std::endl
 					<< project_free(&the_project);
 		}
 		else
 		{
 			ASSERT_TRUE(target_evaluate_by_name(&the_project, &target_name_in_range, verbose))
-					<< full_path << std::endl
+					<< full_path_to_module << std::endl
+					<< full_path_to_script_file << std::endl
 					<< current_directory << std::endl
 					<< target_name << std::endl
 					<< project_free(&the_project);

ant4c.dns.xml
@@ -7,7 +7,8 @@
 
   <choose>
     <when test="${platform::is-unix()}">
-      <loadtasks module="libant4c.dns.so" />
+      <loadtasks module="libant4c.dns.so" failonerror="false" />
+      <loadtasks module="libant4c.dns.dylib" failonerror="false" />
     </when>
     <when test="${platform::is-windows()}">
       <loadtasks module="ant4c.dns.dll" failonerror="false" />

ant4c.net.module.xml
@@ -24,7 +24,14 @@
       </when>
 
       <when test="${platform::is-unix()}">
-        <loadtasks module="libant4c.net.module.so" />
+        <trycatch>
+          <try>
+            <loadtasks module="libant4c.net.module.so" />
+          </try>
+          <catch>
+            <loadtasks module="libant4c.net.module.dylib" />
+          </catch>
+        </trycatch>
       </when>
     </choose>
   </target>

ant4c.regex.xml
@@ -6,7 +6,8 @@
 
   <choose>
     <when test="${platform::is-unix()}">
-      <loadtasks module="libant4c.regex.so" />
+      <loadtasks module="libant4c.regex.so" failonerror="false" />
+      <loadtasks module="libant4c.regex.dylib" unless="${task::exists('regex')}" />
     </when>
     <when test="${platform::is-windows()}">
       <loadtasks module="ant4c.regex.dll" failonerror="false" />

commit 15df1ea90bb5b763ff830b3aac28399f7c4b8433

[tests_load_tasks.cpp] fixed regression from 735341472f6db74a2aecae9e589fd3f7588daff9 commit.
Initial 'const char*' pointer equal to the "" also check it on nullptr
before attempt to create std::string - no matter what result expected.

tests_load_tasks.cpp
@@ -66,7 +66,7 @@ TEST_F(TestLoadTasks, load_tasks_evaluate_task)
 	{
 		expected_return = static_cast<uint8_t>(INT_PARSE(
 				node.node().select_node("return").node().child_value()));
-		const uint8_t* full_path = nullptr;
+		const uint8_t* full_path = reinterpret_cast<const uint8_t*>("");
 
 		for (uint8_t i = 0; i < task_attributes_count; ++i)
 		{
@@ -127,7 +127,7 @@ TEST_F(TestLoadTasks, load_tasks_evaluate_task)
 			continue;
 		}
 
-		const std::string full_path_to_module(reinterpret_cast<const char*>(full_path));
+		const std::string full_path_to_module(full_path ? reinterpret_cast<const char*>(full_path) : "");
 		static const void* the_target = nullptr;
 		const auto returned = load_tasks_evaluate_task(&the_project, the_target, &task_arguments, verbose);
 		//